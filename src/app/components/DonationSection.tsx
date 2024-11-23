448e0baf1ec71e7fa4cadf4b7dd6eeaafbb50410
mateus <mateus.carvalho@paylab.com.br>
2024-11-23 15:46:03 -0300

    improving donation section

--- src/app/components/DonationSection.tsx
+++ src/app/components/DonationSection.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { Github } from "lucide-react";
+import { Github, X } from "lucide-react";
 import { QRCodeCanvas } from "qrcode.react";
 import { useState } from "react";
-import Modal from "./Modal";
 import { Button } from "./ui/button";
 import {
   Card,
@@ -18,8 +17,15 @@ export default function DonationSection() {
   const pixKey = "09243562940"; // Chave Pix
   const pixPayload = `00020126480014BR.GOV.BCB.PIX0113${pixKey}52040000530398654045.005802BR5913Nome do Recebedor6009SAO PAULO62070503***6304`; // Payload Pix
 
+  const handleBackgroundClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      setOpen(false);
+    }
+  };
+
   return (
     <>
+      {/* Donation Section */}
       <Card className="text-center bg-white rounded-lg shadow p-2 w-full">
         <CardHeader>
           <CardTitle className="text-md">Nos apoie com um café!</CardTitle>
@@ -33,52 +39,67 @@ export default function DonationSection() {
           </Button>
         </CardContent>
         <CardFooter className="flex flex-col">
-            <div>
+          <div>
             <h3 className="text-sm font-semibold text-gray-700 mb-2">
               Desenvolvedores
             </h3>
-            </div>
-            <div className="flex flex-col space-y-1">
-              <a
-                href="https://github.com/desenvolvedor2"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center text-black hover:text-gray-800 text-sm"
-              >
-                <Github className="w-4 h-4 mr-2" />
-                phsilva
-              </a>
-              <a
-                href="https://github.com/desenvolvedor1"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center text-black hover:text-gray-800 text-sm"
-              >
-                <Github className="w-4 h-4 mr-2" />
-                devmateuscarvalho
-              </a>
-            </div>
+          </div>
+          <div className="flex flex-col space-y-1">
+            <a
+              href="https://github.com/desenvolvedor2"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center text-black hover:text-gray-800 text-sm"
+            >
+              <Github className="w-4 h-4 mr-2" />
+              phsilva
+            </a>
+            <a
+              href="https://github.com/desenvolvedor1"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center text-black hover:text-gray-800 text-sm"
+            >
+              <Github className="w-4 h-4 mr-2" />
+              devmateuscarvalho
+            </a>
+          </div>
         </CardFooter>
       </Card>
 
-      <Modal
-        open={open}
-        onClose={() => setOpen(false)}
-        title="Contribua com R$5,00"
-      >
-        <div className="flex flex-col items-center justify-center space-y-4">
-          <QRCodeCanvas
-            value={pixPayload}
-            size={200}
-            bgColor={"#ffffff"}
-            fgColor={"#000000"}
-            level={"H"}
-          />
-          <p className="text-sm text-gray-600 text-center">
-            Use o QR Code acima para enviar R$5,00 via Pix.
-          </p>
+      {/* Modal */}
+      {open && (
+        <div
+          className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4"
+          onClick={handleBackgroundClick}
+        >
+          <Card className="relative w-86 max-w-lg bg-white shadow-lg rounded-md">
+            <button
+              onClick={() => setOpen(false)}
+              className="absolute top-2 right-2"
+            >
+              <X width={20} height={20} />
+            </button>
+            <CardHeader>
+              <CardTitle className="text-center">Contribua com R$5,00</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-col items-center justify-center space-y-4">
+                <QRCodeCanvas
+                  value={pixPayload}
+                  size={200}
+                  bgColor={"#ffffff"}
+                  fgColor={"#000000"}
+                  level={"H"}
+                />
+                <p className="text-sm text-gray-600 text-center">
+                  Use o QR Code acima para enviar R$5,00 via Pix.
+                </p>
+              </div>
+            </CardContent>
+          </Card>
         </div>
-      </Modal>
+      )}
     </>
   );
 }
